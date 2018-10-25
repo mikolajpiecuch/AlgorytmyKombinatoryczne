2f868e55f3e2c54717ae6f9c4f85a99c6083f356
Mikolaj Piecuch <mikolajpiecuch1@gmail.com>
2018-10-25 22:46:06 +0200

    Add a simple generation

--- Alg9/Program.cs
+++ Alg9/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Alg9
 {
@@ -6,7 +8,73 @@ namespace Alg9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var perms = Permutation(4, 4);
+            PrintPermutations(perms);
+            Console.ReadKey();
+
         }
+
+        static void PrintPermutations(IEnumerable<int[]> permutations)
+        {
+            foreach (var permutation in permutations)
+            {
+                foreach (var element in permutation)
+                {
+                    Console.Write(element);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static IEnumerable<int[]> Permutation(int length, int level)
+        {
+            if (level == 1)
+            {
+                return new List<int[]>
+                {
+                    new int[] { length }
+                };
+            }
+
+            //permutations.Trans()
+            var permutations = Permutation(length, level - 1);
+            var newPermutations = new List<int[]>();
+            foreach (var permutation in permutations)
+            {
+                var tempPerm = new int[level];
+                tempPerm[0] = length - level + 1;
+                for (int i = 0; i < permutation.Length; i++)
+                {
+                    tempPerm[i + 1] = permutation[i];
+                }
+                
+                newPermutations.Add(tempPerm.Clone() as int[]);
+                for (int i = 0; i < tempPerm.Length - 1; i++)
+                {
+                    tempPerm.Trans(i, i + 1);
+                    newPermutations.Add(tempPerm.Clone() as int[]);
+                }
+
+                //foreach (var element in permutation)
+                //{
+                //    newPermutations.Add(element);
+                //}
+                
+            }
+            return newPermutations;
+        }
+
+    }
+
+    public static class Extension
+    {
+        public static T[] Trans<T>(this T[] array, int firstIndex, int secondIndex)
+        {
+            var firstElement = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = firstElement;
+            return array;
+        }
+
     }
 }
