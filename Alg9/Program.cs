277103b64b765df065d2e6b2b9819deb41f248f1
Mikolaj Piecuch <mikolajpiecuch1@gmail.com>
2018-10-25 21:28:59 +0200

    Add project files.

---
+++ Alg9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alg9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
